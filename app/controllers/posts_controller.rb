ce4c356f7aef12f71787af5e099d305631148236
onigiriumeboshihari <koyamada89179@gol.com>
2021-03-03 16:25:55 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,14 @@
 class PostsController < ApplicationController
+  
   def index
-    @post = Posts.all
+    @posts = Posts.order(id: "DESC")
   end
 
-  def new
-    
+  #def new
+  #end
+
+  def create
+    Post.create(content: params[:content])
+    redirect_to action: :index
   end
 end
